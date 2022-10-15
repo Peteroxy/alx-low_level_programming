68a6ae13534352e47fb6b58a641751c9611632c3
Adunmoye Peter <adunmoyepeter111@gmail.com>
2022-10-14 20:01:44 -0700

    my commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,9 +5,10 @@
  * 10 starting from 0, followed by a new line
  * Return: Always 0 (Success)
  */
+
+int main(void)
 {
-	int main(void)
-		int n;
+	int n;
 
 	for (n = 0; n < 10; n++)
 		printf("%d", n);
