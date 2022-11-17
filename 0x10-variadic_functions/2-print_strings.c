6362750e083926f860903ce1a2469652c43b52bd
adunmoye prter <adunmoyepeter111@gmail.com>
2022-11-17 04:01:50 -0800

    my commit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,12 +18,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		str = va_arg(list, char *);
 		if (!str)
-			if (!separator)
-				printf("%s", str);
-			else if (separator && i == 0)
-				printf("%s", str);
-			else
-				printf("%s%s", separator, str);
+			str = "(nil)";
+		if (!separator)
+			printf("%s", str);
+		else if (separator && i == 0)
+			printf("%s", str);
+		else
+			printf("%s%s", separator, str);
 	}
 
 	printf("\n");
